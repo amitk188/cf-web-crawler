3bbf1ae9eb74614cd350018113ee05e48e2b2a58
Amit Kumar <66002052+amitk188@users.noreply.github.com>
2021-02-09 16:54:56 +0530

    file upload

---
+++ cfwebcrawl/cfwebcrawl/spiders/cf_crawler.py
@@ -0,0 +1,26 @@
+import scrapy
+from itertools import groupby
+from urllib.parse import urljoin
+from scrapy.selector import Selector
+from ..items import CfwebcrawlItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class Cfwebcrawl(CrawlSpider):
+    name = 'spider'
+    #start_urls = ['http://www.cure.fit']
+    handle_httpstatus_list = [404]
+    def __init__(self, *args, **kwargs): 
+        super(Cfwebcrawl, self).__init__(*args, **kwargs) 
+        self.start_urls = [kwargs.get('start_url')]
+        self.allowed_domains = ['cure.fit']
+        Cfwebcrawl.rules = [Rule(LinkExtractor(allow=(), deny=(['blog.cure.fit']), allow_domains=self.allowed_domains), callback='parse_items', follow=True)]
+        super(Cfwebcrawl, self)._compile_rules()
+
+    def parse_items(self, response):
+        if response.status == 404:
+            item = CfwebcrawlItem()
+            item['url'] = response.url
+            item['referer'] = (response.request.headers.get('Referer')).decode('UTF-8')
+            item['status'] = response.status
+            yield item
