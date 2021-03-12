139dbfed57c32b78df562ee65881a074b8f6eaa9
Amit Kumar <66002052+amitk188@users.noreply.github.com>
2021-03-12 16:56:20 +0530

    create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import re
+import sys
+import json
+import requests
+import argparse
+import numpy as np
+#import urllib.request
+from easyocr import Reader
+from bs4 import BeautifulSoup
+from difflib import SequenceMatcher
+import AdvancedHTMLParser
+from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession
+from urllib.request import urljoin 
+from urllib.request import urlparse
